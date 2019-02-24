2bec76b9106717ed4b0f84d9b8f4d19fc607f8ad
Sano75 <43172203+Sano75@users.noreply.github.com>
2019-02-24 18:58:21 +0100

    New version, results shows i double
    Fixed the result so it show more accurate result,

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,97 +26,120 @@ public class Controller implements Initializable {
     @FXML private Button Minus;
     @FXML private Button Equals;
     @FXML private Button CC;
-    
-    Double firstNum;
-    Double secondNum;
-    Double result;
+    @FXML private Text displayNumber;
+
+
+    StringBuilder numberAdding;
+    Integer firstNum;
+    Integer secondNum;
+    double result;
     String Operation;
     String Answer;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         Number0.setOnAction(event ->{
             String text = displayNumber.getText();
-            text = text +0;
+            text = text + "0";
             displayNumber.setText(text);
+
         });
         Number1.setOnAction(event ->{
             String text = displayNumber.getText();
-            text = text +"1";
+            text = text + "1";
             displayNumber.setText(text);
         });
         Number2.setOnAction(event ->{
             String text = displayNumber.getText();
-            text = text +"2";
+            text = text + "2";
             displayNumber.setText(text);
         });
         Number3.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"3";
+            String text = displayNumber.getText();
+            text = text + "3";
             displayNumber.setText(text);
         });
         Number4.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"4";
+            String text = displayNumber.getText();
+            text = text + "4";
             displayNumber.setText(text);
+
         });
         Number5.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"5";
+            String text = displayNumber.getText();
+            text = text + "5";
             displayNumber.setText(text);
         });
         Number6.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"6";
+            String text = displayNumber.getText();
+            text = text + "6";
             displayNumber.setText(text);
         });
 
         Number7.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"7";
+            String text = displayNumber.getText();
+            text = text + "7";
             displayNumber.setText(text);
         });
         Number8.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"8";
+            String text = displayNumber.getText();
+            text = text + "8";
             displayNumber.setText(text);
         });
         Number9.setOnAction(event ->{
-              String text = displayNumber.getText();
-            text = text +"9";
+            String text = displayNumber.getText();
+            text = text + "9";
             displayNumber.setText(text);
         });
         Slash.setOnAction(event ->{
-            //here the code of what this number will do if you press it
+            String text = displayNumber.getText();
+            firstNum = Integer.parseInt(text);
+            displayNumber.setText("");
+            Operation = "Slash";
         });
         Star.setOnAction(event ->{
-            //here the code of what this number will do if you press it
+            String text = displayNumber.getText();
+            firstNum = Integer.parseInt(text);
+            displayNumber.setText("");
+            Operation = "Star";
         });
         Plus.setOnAction(event ->{
-            //here the code of what this number will do if you press it
-            firstNum = double.parseDouble(txtDisplay.getText());
-            txtDisplay.setTExt(“”);
-            Operation = “+”;
+            String text = displayNumber.getText();
+            firstNum = Integer.parseInt(text);
+            displayNumber.setText("");
+            Operation = "+";
         });
         Minus.setOnAction(event ->{
-            //here the code of what this number will do if you press it
-            firstNum = double.parseDouble(txtDisplay.getText());
-            txtDisplay.setTExt(“”);
-            Operation = “-”;
+            String text = displayNumber.getText();
+            firstNum = Integer.parseInt(text);
+            displayNumber.setText("");
+            Operation = "-";
+
 
         });
         Equals.setOnAction(event ->{
-            //here the code of what this number will do if you press it
+            double text = Double.parseDouble(displayNumber.getText());
+            if(Operation == "+"){
+                result = firstNum + text;
+            }
+            else if(Operation == "-"){
+                result = firstNum - text;
+            }
+            else if(Operation == "Star"){
+                result = firstNum * text;
+            }
+            else if(Operation == "Slash"){
+                result = firstNum / text;
+            }
+
+            displayNumber.setText(Double.toString(result));
         });
         CC.setOnAction(event ->{
-            //here the code of what this number will do if you press it
+            displayNumber.setText("");
+
         });
     }
-
-
-}
-
-
 
 }
